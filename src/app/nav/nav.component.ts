e3ac917ae19383760cfebb3a77d015e14eeaab79
Praful Kumar <you@example.com>
2019-05-31 13:15:19 +0530

    updated ui of navbar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,20 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+declare var $: any;
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements AfterViewInit {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
+  ngAfterViewInit() {
+    $(document).ready(function () {
+      $('nav ul li a:not(:only-child)').click(function (e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        $('.dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+      });
+      $('html').click(function () {
+        $('.nav-dropdown').hide();
+      });
+      $('#nav-toggle').click(function () {
+        $('nav ul').slideToggle();
+      });
+      $('#nav-toggle').on('click', function () {
+        this.classList.toggle('active');
+      });
+    });
+  }
 }
